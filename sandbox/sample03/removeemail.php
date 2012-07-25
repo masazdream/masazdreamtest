e31dba10e8ebc3a75a96dedabc3fd8f5cba55652
masahiroimai <masazdream@gmail.com>
2012-07-25 23:33:52 +0900

    sample03:complete sample04:complete

--- sandbox/sample03/removeemail.php
+++ sandbox/sample03/removeemail.php
@@ -1,23 +1,43 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>削除しました</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Make Me Elvis：メールアドレス削除</title>
+	<link rel="stylesheet" type="text/css" href="style.css" />
 </head>
     <body>
+    <img src="blankface.jpg" width="161" height="350" alt="" style="float:right" />
+	<img name="elvislogo" src="elvislogo.gif" width="229" height="32" border="0" alt="Make Me Elvis" />
+	<p>削除するメールアドレスを選択してください。</p>
+	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
     <?php
 	    $dbc = mysqli_connect('localhost', 'masahiro', 'passwd', 'elvis_store')
 	    or die('error: can not connect to database');
 	    
-	    $email = $_POST['email'];
-	    $query = "delete from email_list where email='$email'";
+		if(isset($_POST['submit'])){
+			foreach($_POST['todelete'] as $delete_id){
+				$query = "delete from email_list where id=$delete_id";
+				mysqli_query($dbc, $query)
+				or die('failed to query database for delete');
+			}
+			echo 'deleted customer\'s address <br />';
+		}
 	    
-	    mysqli_query($dbc, $query)
+	    
+	    $query = "select * from email_list";
+	    $result = mysqli_query($dbc, $query)
 	    or die('erro: failed query to database');
-	    
-	    echo 'delete ' . $email . '.';
-	    
+
+	    while($row = mysqli_fetch_array($result)){
+	    	echo '<input type="checkbox" value="' . $row['id'] . '" name="todelete[]" />';
+	    	echo $row['last_name'];
+	    	echo ' ' . $row['first_name'];
+	    	echo ' ' . $row['email'];
+	    	echo '<br />';
+	    }
 	    mysqli_close($dbc);
 	?>
+		<input type="submit" name="submit" value="削除" />
+	</form>
     </body>
 </html>
--- sandbox/sample03/sendemail.php
+++ sandbox/sample03/sendemail.php
@@ -2,32 +2,73 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>メッセージの送信完了</title>
+<title>Make Me Elvis：メール送信</title>
 </head>
     <body>
+    <img src="blankface.jpg" width="161" height="350" alt="" style="float:right" />
+  				<img name="elvislogo" src="elvislogo.gif" width="229" height="32" border="0" alt="Make Me Elvis" />
+ 				<p><strong>管理用：</strong>管理者以外使用禁止<br />
+ 				 メールを書いてメーリングリストのメンバに送りますぜ。</p>
     <?php
-		$from = 'test@example.com';
-		$subject = $_POST['subject'];
-		$text = $_POST['elvismail'];
-		
-		$dbc = mysqli_connect('localhost', 'masahiro', 'passwd', 'elvis_store')
-		or die('error: can not connect to database');
-		
-		$query = "select * from email_list";
-		$result = mysqli_query($dbc, $query)
-		or die('error: failed query to database');
-		
-		while($row = mysqli_fetch_array($result)){
-			$last_name = $row['last_name'];
-			$first_name = $row['first_name'];
-			$msg = "$last_name $first_name さん, \n $text";
-			$to = $row['email'];
+	    if (isset($_POST['submit'])){
+			$from = 'test@example.com';
+			$subject = $_POST['subject'];
+			$text = $_POST['elvismail'];
 			
-			mb_internal_encoding("UTF-8");
-			mb_send_mail($to, $subject, $msg, 'From: ' . $from);
-			echo 'sent mail to ' . $to . '<br />';
+			$output_form = false;
+			
+			if(empty($subject) && empty($text)){
+				$output_form = true;
+				echo '件名と本文を入力してください。<br />';
+			}
+			
+			if(empty($subject) && !empty($text)){
+				$output_form = true;
+				echo '件名を入力してください。<br />';
+			}
+			
+			if(!empty($subject) && empty($text)){
+				$output_form = true;
+				echo '本文を入力してください。<br />';
+			}
+	    }else{
+	    	$output_form = true;
+	    	$subject = '';
+	    	$text = '';
+	    }
+		
+		if(!empty($subject) && !empty($text)){
+			$dbc = mysqli_connect('localhost', 'masahiro', 'passwd', 'elvis_store')
+			or die('error: can not connect to database');
+			
+			$query = "select * from email_list";
+			$result = mysqli_query($dbc, $query)
+			or die('error: failed query to database');
+			
+			while($row = mysqli_fetch_array($result)){
+				$last_name = $row['last_name'];
+				$first_name = $row['first_name'];
+				$msg = "$last_name $first_name さん, \n $text";
+				$to = $row['email'];
+				
+				mb_internal_encoding("UTF-8");
+				mb_send_mail($to, $subject, $msg, 'From: ' . $from);
+				echo 'sent mail to ' . $to . '<br />';
+			}
+			mysqli_close($dbc);
 		}
-		mysqli_close($dbc);
+		
+		if($output_form){
+			?>
+				<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+			    <label for="subject">メールの件名：</label><br />
+			    <input id="subject" name="subject" type="text" size="30" value="<?php echo $subject; ?>"/><br />
+			    <label for="elvismail">メール本文：</label><br />
+			    <textarea id="elvismail" name="elvismail" rows="8" cols="40"><?php echo $text; ?></textarea><br />
+			   	<input type="submit" name="submit" value="送信" />
+	 		 	</form>
+			<?php
+		}
 	?>
     </body>
 </html>
